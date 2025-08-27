986991c13e407f1fab530ece38534eabdc534d10
w7ldan <wildanhidayatirl@gmail.com>
2025-08-27 15:38:30 +0700

    Tutorial 0

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://wildan-anshari-footballnews.pbp.cs.ui.ac.id/"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "wildan-anshari-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
