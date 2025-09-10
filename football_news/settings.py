01f8f587f5a52794c70601ffe90e301ea8cda9bb
w7ldan <wildanhidayatirl@gmail.com>
2025-09-10 15:23:20 +0700

    <pesan_commit>

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,6 +34,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "wildan-anshari-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://wildan-anshari-footballnews.pbp.cs.ui.ac.id"
+]
+
 
 # Application definition
 
@@ -62,7 +66,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
